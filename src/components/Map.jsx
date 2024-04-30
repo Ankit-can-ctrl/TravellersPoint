b9b32781d1a96dfe3debaec80c819072d605325a
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-04-30 23:39:11 +0530

    displaying city marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,9 +2,11 @@ import styles from "./Map.module.css";
 // import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../Contexts/CitiesContext";
 /*eslint-disable*/
 function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
   // const navigate = useNavigate();
   // const [searchParams, setSearchParams] = useSearchParams();
   // const lat = searchParams.get("lat");
@@ -21,11 +23,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
